83382500970056e4a80a79de98e0a7a0a4a3b44e
Anthony Libardi <libardi42@gmail.com>
2025-02-25 13:26:32 -0500

    rename reg

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,4 +1,4 @@
-mod registers;
+pub mod registers;
 
 use registers::*;
 use std::{fmt, fmt::Display, fmt::Formatter};
@@ -10,18 +10,18 @@ pub struct CPU  {
     // Stack pointer
     pub sp: u16,
     // The 8 main registers
-    pub reg: Registers,
+    pub regs: Registers,
 }
 
 impl Display for CPU {
     fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
-        let Registers {a, f, b, c, d, e, h, l} = &self.reg;
+        let Registers {a, f, b, c, d, e, h, l} = &self.regs;
         write!(formatter, "+--------------------------+\n")?;
         write!(formatter, "| PC: 0x{:0>4X}    SP: 0x{:0>4X} |\n", self.pc, self.sp)?;
         write!(formatter, "| A:  0x{a:0>2X}      F:  0x{f:0>2X}   |\n")?;
         write!(formatter, "| B:  0x{b:0>2X}      C:  0x{c:0>2X}   |\n")?;
         write!(formatter, "| D:  0x{d:0>2X}      E:  0x{e:0>2X}   |\n")?;
         write!(formatter, "| H:  0x{h:0>2X}      L:  0x{l:0>2X}   |\n")?;
-        write!(formatter, "+--------------------------+\n")
+        write!(formatter, "+--------------------------+")
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 mod cpu;
+
 use cpu::*;
 
 fn main() {
     let mut cpu = CPU::default();
     cpu.pc = 0xDEAD;
     cpu.sp = 0xBEEF;
-    cpu.reg.set_af(0xDEAD);
-    cpu.reg.set_bc(0xBEEF);
-    cpu.reg.set_de(0xF00D);
-    cpu.reg.set_hl(0xCAFE);
+    cpu.regs.set_af(0xDEAD);
+    cpu.regs.set_bc(0xBEEF);
+    cpu.regs.set_de(0xF00D);
+    cpu.regs.set_hl(0xCAFE);
     println!("{cpu}");
 }
