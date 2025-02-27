19dcb3faed80587652877dd82003feaf7b3b8329
Anthony Libardi <libardi42@gmail.com>
2025-02-26 21:17:09 -0500

    in the middle of replacing constant instructions with extra enums

--- src/gba/cpu.rs
+++ src/gba/cpu.rs
@@ -3,7 +3,7 @@ pub mod instructions;
 
 use registers::Registers;
 use instructions::{*, Instruction::*};
-use std::{fmt, fmt::Display, fmt::Formatter};
+use std::fmt::{self, Display, Formatter};
 use super::MMU;
 
 #[derive(Debug)]
@@ -14,21 +14,6 @@ pub struct CPU  {
     pub sp: u16,
     // The 8 main registers
     pub regs: Registers,
-}
-
-macro_rules! value_at_ArgR8_to_a {
-    ($callr:expr, $mmu:expr, $func:ident, $match_on:expr $(, $other_args:expr)*) => {
-        match $match_on {
-            ArgR8::B => $callr.regs.a = $callr.$func($callr.regs.b $(, $other_args)*),
-            ArgR8::C => $callr.regs.a = $callr.$func($callr.regs.c $(, $other_args)*),
-            ArgR8::D => $callr.regs.a = $callr.$func($callr.regs.d $(, $other_args)*),
-            ArgR8::E => $callr.regs.a = $callr.$func($callr.regs.e $(, $other_args)*),
-            ArgR8::H => $callr.regs.a = $callr.$func($callr.regs.h $(, $other_args)*),
-            ArgR8::L => $callr.regs.a = $callr.$func($callr.regs.l $(, $other_args)*),
-            ArgR8::MHL => $callr.regs.a = $callr.$func($mmu.read_byte($callr.regs.get_hl()) $(, $other_args)*),
-            ArgR8::A => $callr.regs.a = $callr.$func($callr.regs.a $(, $other_args)*),
-        }
-    };
 }
 
 impl CPU {
@@ -42,18 +27,68 @@ impl CPU {
         self.regs = Registers::new();
     }
 
-    fn add_8bit(&mut self, value: u8, with_carry: bool) -> u8 {
+    fn get_value_at_r8(&self, mmu: &MMU, target: ArgR8) -> u8 {
+        match target {
+            ArgR8::B => self.regs.b,
+            ArgR8::C => self.regs.c,
+            ArgR8::D => self.regs.d,
+            ArgR8::E => self.regs.e,
+            ArgR8::H => self.regs.h,
+            ArgR8::L => self.regs.l,
+            ArgR8::MHL => mmu.read_byte(self.regs.get_hl()),
+            ArgR8::A => self.regs.a,
+            ArgR8::CONST(value) => value,
+        }
+    }
+
+    fn set_value_at_r8(&mut self, mmu: &mut MMU, target: ArgR8, value: u8) {
+        match target {
+            ArgR8::B => self.regs.b = value,
+            ArgR8::C => self.regs.c = value,
+            ArgR8::D => self.regs.d = value,
+            ArgR8::E => self.regs.e = value,
+            ArgR8::H => self.regs.h = value,
+            ArgR8::L => self.regs.l = value,
+            ArgR8::MHL => mmu.write_byte(self.regs.get_hl(), value),
+            ArgR8::A => self.regs.a = value,
+            ArgR8::CONST(_) => panic!("Constant not allowed here"),
+        }
+    }
+
+    fn guard_no_const(arg_r8: ArgR8) {
+        match arg_r8 {
+            ArgR8::CONST(_) => panic!("Constant not allowed here"),
+            _ => ()
+        }
+    }
+
+    fn guard_only_const(arg_r8: ArgR8) {
+        match arg_r8 {
+            ArgR8::CONST(_) => (),
+            _ => panic!("Only constants allowed here"),
+        }
+    }
+
+    fn add_8bit(&mut self, mmu: &MMU, target: ArgR8, with_carry: bool) {
+        // Load value from target
+        let value = self.get_value_at_r8(mmu, target);
+
+        // Calculate
         let cv = if with_carry && self.regs.getf_carry() {1} else {0};
         let (result, overflow1) = self.regs.a.overflowing_add(value);
         let (result, overflow2) = result.overflowing_add(cv);
         let nibble_sum = (self.regs.a & 0xF) + (value & 0xF) + cv;
+
+        // Set flags
         self.regs.set_all_flags(
             result == 0,
             false,
             nibble_sum > 0xF,
             overflow1 || overflow2
         );
-        result
+
+        // Write result to A
+        self.regs.a = result;
     }
 
     fn sub_8bit(&mut self, value: u8, with_carry: bool) -> u8 {
@@ -74,10 +109,95 @@ impl CPU {
 impl CPU {
     pub fn execute(&mut self, mmu: &mut MMU, instruction: Instruction) {
         match instruction {
+            // Load (LD_dest_source)
+            LD_r8_r8(dest, src) => todo!(),
+            LD_r16_n16(dest, value) => todo!(),
+            LD_mr16_a(dest_address) => todo!(),
+            LDH_mn8_a(dest_address) => todo!(),
+            LDH_mc_a => todo!(),
+            LD_a_mr16(src_address) => todo!(),
+            LDH_a_mn16(src_address) => todo!(),
+            LDH_a_mc => todo!(),
+            
+            // 8-bit arithmetic
+            ADC_a_r8(src) => self.add_8bit(mmu, src, true),
+            ADD_a_r8(src) => self.add_8bit(mmu, src, false),
+            CP_a_r8(src) => todo!(),
+            DEC_r8(target) => todo!(),
+            INC_r8(target) => todo!(),
+            SBC_a_r8(src) => todo!(),
+            SUB_a_r8(src) => todo!(),
+            
+            // 16-bit arithmetic
+            ADD_hl_r16(arg_r16) => todo!(),
+            DEC_r16(arg_r16) => todo!(),
+            INC_r16(arg_r16) => todo!(),
+            
+            // Bitwise logic
+            AND_a_r8(arg_r8) => todo!(),
+            CPL => todo!(),
+            OR_a_r8(arg_r8) => todo!(),
+            XOR_a_r8(arg_r8) => todo!(),
+            
+            // Bit flags
+            BIT_u3_r8(arg_u3, arg_r8) => todo!(),
+            RES_u3_r8(arg_u3, arg_r8) => todo!(),
+            SET_u3_r8(arg_u3, arg_r8) => todo!(),
+            
+            // Bit shift
+            RL_r8(arg_r8) => todo!(),
+            RLA => todo!(),
+            RLC_r8(arg_r8) => todo!(),
+            RLCA => todo!(),
+            RR_r8(arg_r8) => todo!(),
+            RRA => todo!(),
+            RRC_r8(arg_r8) => todo!(),
+            RRCA => todo!(),
+            SLA_r8(arg_r8) => todo!(),
+            SRA_r8(arg_r8) => todo!(),
+            SRL_r8(arg_r8) => todo!(),
+            SWAP_r8(arg_r8) => todo!(),
+            
+            // Jumps and subroutines
+            CALL_n16(_) => todo!(),
+            CALL_cc_n16(arg_cond, _) => todo!(),
+            JP_hl => todo!(),
+            JP_n16(_) => todo!(),
+            JP_cc_n16(arg_cond, _) => todo!(),
+            JR_n16(_) => todo!(),
+            JR_cc_n16(arg_cond, _) => todo!(),
+            RET_cc(arg_cond) => todo!(),
+            RET => todo!(),
+            RETI => todo!(),
+            RST_vec(arg_vec) => todo!(),
+            
+            // Carry flag
+            CCF => todo!(),
+            SCF => todo!(),
+            
+            // Stack manipulation
+            ADD_hl_sp => todo!(),
+            ADD_sp_e8(_) => todo!(),
+            DEC_sp => todo!(),
+            INC_sp => todo!(),
+            LD_sp_n16(_) => todo!(),
+            LD_mn16_sp(_) => todo!(),
+            LD_hl_sp_plus_e8(_) => todo!(),
+            LD_sp_hl => todo!(),
+            POP_af => todo!(),
+            POP_r16(arg_r16_stk) => todo!(),
+            PUSH_af => todo!(),
+            PUSH_r16(arg_r16_stk) => todo!(),
+            
+            // Interrupt-related
+            DI => todo!(),
+            EI => todo!(),
+            HALT => todo!(),
+            
+            // Miscellaneous
+            DAA => todo!(),
             NOP => (),
-            ADD_a_r8(target) => value_at_ArgR8_to_a!(self, mmu, add_8bit, target, false),
-            ADC_a_r8(target) => value_at_ArgR8_to_a!(self, mmu, add_8bit, target, true),
-            _ => todo!()
+            STOP => todo!(),
         }
     }
 }
--- src/gba/cpu/instructions.rs
+++ src/gba/cpu/instructions.rs
@@ -6,30 +6,22 @@
 pub enum Instruction {
     // Load (LD_dest_source)
     LD_r8_r8(ArgR8, ArgR8),
-    LD_r8_n8(ArgR8, u8),
     LD_r16_n16(ArgR16, u16),
     LD_mr16_a(ArgR16MEM),
-    LD_mn16_a(u16),
     LDH_mn8_a(u8),
     LDH_mc_a,
     LD_a_mr16(ArgR16MEM),
-    LD_a_mn16(u16),
     LDH_a_mn16(u8),
     LDH_a_mc,
 
     // 8-bit arithmetic
     ADC_a_r8(ArgR8),
-    ADC_a_n8(u8),
     ADD_a_r8(ArgR8),
-    ADD_a_n8(u8),
     CP_a_r8(ArgR8),
-    CP_a_n8(u8),
     DEC_r8(ArgR8),
     INC_r8(ArgR8),
     SBC_a_r8(ArgR8),
-    SBC_a_n8(u8),
     SUB_a_r8(ArgR8),
-    SUB_a_n8(u8),
 
     // 16-bit arithmetic
     ADD_hl_r16(ArgR16),
@@ -38,12 +30,9 @@ pub enum Instruction {
 
     // Bitwise logic
     AND_a_r8(ArgR8),
-    AND_a_n8(u8),
     CPL,
     OR_a_r8(ArgR8),
-    OR_a_n8(u8),
     XOR_a_r8(ArgR8),
-    XOR_a_n8(u8),
 
     // Bit flags
     BIT_u3_r8(ArgU3, ArgR8),
@@ -115,12 +104,16 @@ pub enum ArgR8 {
     L,
     MHL,
     A,
+
+    CONST(u8),
 }
 
 pub enum ArgR16 {
     BC,
     DE,
     HL,
+
+    CONST(u16),
 }
 
 pub enum ArgR16STK {
@@ -135,6 +128,8 @@ pub enum ArgR16MEM {
     DE,
     HLI,
     HLD,
+    
+    CONST(u16),
 }
 
 pub enum ArgU3 {
@@ -166,98 +161,9 @@ pub enum ArgCOND {
     C,
 }
 
-// pub fn inst_len(inst: Instruction) -> u8 {
-//     use Instruction::*;
-//     match inst {
-//         // Load (LD_dest_source)
-//         LD_r8_r8(_, _) => 1,
-//         LD_r8_n8(_, _) => 1,
-//         LD_r16_n16(_, _) => 1,
-//         LD_mr16_a(_) => 1,
-//         LD_mn16_a(_) => 1,
-//         LDH_mn8_a(_) => 1,
-//         LDH_mc_a => 1,
-//         LD_a_mr16(_) => 1,
-//         LD_a_mn16(_) => 1,
-//         LDH_a_mn16(_) => 1,
-//         LDH_a_mc => 1,
-//         // 8-bit arithmetic
-//         ADC_a_r8(_) => 1,
-//         ADC_a_n8(_) => 1,
-//         ADD_a_r8(_) => 1,
-//         ADD_a_n8(_) => 1,
-//         CP_a_r8(_) => 1,
-//         CP_a_n8(_) => 1,
-//         DEC_r8(_) => 1,
-//         INC_r8(_) => 1,
-//         SBC_a_r8(_) => 1,
-//         SBC_a_n8(_) => 1,
-//         SUB_a_r8(_) => 1,
-//         SUB_a_n8(_) => 1,
-//         // 16-bit arithmetic
-//         ADD_hl_r16(_) => 1,
-//         DEC_r16(_) => 1,
-//         INC_r16(_) => 1,
-//         // Bitwise logic
-//         AND_a_r8(_) => 1,
-//         AND_a_n8(_) => 1,
-//         CPL => 1,
-//         OR_a_r8(_) => 1,
-//         OR_a_n8(_) => 1,
-//         XOR_a_r8(_) => 1,
-//         XOR_a_n8(_) => 1,
-//         // Bit flags
-//         BIT_u3_r8(_, _) => 1,
-//         RES_u3_r8(_, _) => 1,
-//         SET_u3_r8(_, _) => 1,
-//         // Bit shift
-//         RL_r8(_) => 1,
-//         RLA => 1,
-//         RLC_r8(_) => 1,
-//         RLCA => 1,
-//         RR_r8(_) => 1,
-//         RRA => 1,
-//         RRC_r8(_) => 1,
-//         RRCA => 1,
-//         SLA_r8(_) => 1,
-//         SRA_r8(_) => 1,
-//         SRL_r8(_) => 1,
-//         SWAP_r8(_) => 1,
-//         // Jumps and subroutines
-//         CALL_n16(_) => 1,
-//         CALL_cc_n16(_, _) => 1,
-//         JP_hl => 1,
-//         JP_n16(_) => 1,
-//         JP_cc_n16(_, _) => 1,
-//         JR_n16(_) => 1,
-//         JR_cc_n16(_, _) => 1,
-//         RET_cc(_) => 1,
-//         RET => 1,
-//         RETI => 1,
-//         RST_vec(_) => 1,
-//         // Carry flag
-//         CCF => 1,
-//         SCF => 1,
-//         // Stack manipulation
-//         ADD_hl_sp => 1,
-//         ADD_sp_e8(_) => 1,
-//         DEC_sp => 1,
-//         INC_sp => 1,
-//         LD_sp_n16(_) => 1,
-//         LD_mn16_sp(_) => 1,
-//         LD_hl_sp_plus_e8(_) => 1,
-//         LD_sp_hl => 1,
-//         POP_af => 1,
-//         POP_r16(_) => 1,
-//         PUSH_af => 1,
-//         PUSH_r16(_) => 1,
-//         // Interrupt-related
-//         DI => 1,
-//         EI => 1,
-//         HALT => 1,
-//         // Miscellaneous
-//         DAA => 1,
-//         NOP => 1,
-//         STOP => 1
-//     }
-// }
+pub fn inst_len(inst: Instruction) -> u8 {
+    use Instruction::*;
+    match inst {
+        _ => todo!()
+    }
+}
