3c49e7c54a30a2565af272b51004d9f825738768
Anthony Libardi <libardi42@gmail.com>
2025-03-02 18:54:33 -0500

    test program

--- src/gba.rs
+++ src/gba.rs
@@ -26,9 +26,14 @@ impl GBA {
         }
     }
 
-    pub fn run(&mut self, debug_print: bool /*, ROM */) {
+    pub fn load(&mut self, rom: &[u8]) {
+        for (i, b) in rom.iter().enumerate() {
+            self.mmu.set(i as u16, *b);
+        }
+    }
+
+    pub fn run(&mut self, debug_print: bool) {
         // TODO: boot sequence
-        // TODO: load ROM
         while !self.cpu.terminate {
             let (inst, inst_length) = decode(&self.mmu, self.cpu.pc);
             self.cpu.execute(&mut self.mmu, inst, inst_length);
@@ -36,22 +41,6 @@ impl GBA {
                 println!("{inst:?}, {inst_length} bytes\n{self}\n\n");
             }
         }
-    }
-
-    pub fn test(&mut self) {
-        let prog = [
-            0x01, 0xAD, 0xDE, // Write 0xDEAD into BC
-            0x80, // A += B (0xDE)
-            0x81, // A += C (0x8B)
-            0xEA, 0xAD, 0xDE, // Write A to [0xDEAD]
-            0xEC, // Terminate
-        ];
-
-        for (i, b) in prog.iter().enumerate() {
-            self.mmu.set(i as u16, *b);
-        }
-
-        self.run(true);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,15 @@ use gba::*;
 
 fn main() {
     let mut gba = GBA::new();
-    gba.test();
+
+    let prog = [
+        0x01, 0xAD, 0xDE, // Write 0xDEAD into BC
+        0x80, // A += B (0xDE)
+        0x81, // A += C (0x8B)
+        0xEA, 0xAD, 0xDE, // Write A to [0xDEAD]
+        0xEC, // Terminate
+    ];
+
+    gba.load(&prog);
+    gba.run(true);
 }
