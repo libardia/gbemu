13453f96bcd5e8c60911540bb4bdec15ef2a839a
Anthony Libardi <libardi42@gmail.com>
2025-03-08 20:03:17 -0500

    Boot rom runs, and successfully passes operation to main program

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,33 @@ mod mmu_basic;
 mod ppu;
 mod ppu_basic;
 
+fn make_dummy_header() -> [u8; 0x150] {
+    const LOGO: [u8; 48] = [
+        0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00, 0x0B, 0x03, 0x73, 0x00, 0x83, 0x00, 0x0C, 0x00,
+        0x0D, 0x00, 0x08, 0x11, 0x1F, 0x88, 0x89, 0x00, 0x0E, 0xDC, 0xCC, 0x6E, 0xE6, 0xDD, 0xDD,
+        0xD9, 0x99, 0xBB, 0xBB, 0x67, 0x63, 0x6E, 0x0E, 0xEC, 0xCC, 0xDD, 0xDC, 0x99, 0x9F, 0xBB,
+        0xB9, 0x33, 0x3E,
+    ];
+
+    let mut header = [0; 0x150];
+
+    header[0x101] = 0xC3;
+    header[0x102] = 0x50;
+    header[0x103] = 0x01;
+
+    for (i, b) in LOGO.iter().enumerate() {
+        header[0x104 + i] = *b;
+    }
+
+    let mut checksum = 0u8;
+    for a in 0x134..=0x14C {
+        checksum = checksum.wrapping_sub(header[a]).wrapping_sub(1);
+    }
+    header[0x14D] = checksum;
+
+    header
+}
+
 #[allow(unused)]
 fn main() {
     type MMU = BasicMMU;
@@ -57,21 +84,25 @@ fn main() {
         0x06, 0x05, // Load 0x05 into B; 5 in BCD
         0x90, // A -= B (0x10 - 0x05 = 0x0B, in BCD: 0x10 - 0x05 = 0x05)
         0x27, // DAA (convert A into BCD)
+        0xED, // Print
         0xEC, // Terminate
     ];
 
     let infinite_loop = [0x20, -2i8 as u8];
 
-    let breakpoints = [0xC];
+    let breakpoints = [0x150 + 16];
 
-    // gb.set_debug_mode(true);
-    gb.set_breakpoints(&breakpoints);
+    gb.set_debug_mode(true);
+    // gb.set_breakpoints(&breakpoints);
 
     // gb.load_rom_file(
     //     r"C:\Users\libar\Projects\rust\gbemu\test-roms\blargg\cpu_instrs\cpu_instrs.gb",
     // );
     // gb.load_rom(0x100, &infinite_loop);
+
+    gb.load_rom(0, &make_dummy_header());
     gb.load_rom(0, &mmu::BOOT_ROM);
+    gb.load_rom(0x150, &binary_coded_decimal);
 
     gb.execute_at(0);
 }
