15756825f1e20e9e90a5396bc9209b7f75586dbb
Anthony Libardi <libardi42@gmail.com>
2025-11-16 14:44:54 -0500

    Oops, last commit was an accident. Added file generated from the reference on gbdev.io, and a function to compare my opcodes to it.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
 use crate::gb::cpu::optable::OP_TABLE;
+use std::fs;
 
 mod gb;
 
 fn main() {
-    let b = 0x1A;
-    println!("OP[{b:#X}]:\n\n{:X?}", OP_TABLE[b]);
+    test_opcodes();
+}
+
+fn test_opcodes() {
+    let mut codes_from_file: [&str; 0x100] = [""; 0x100];
+    let contents = fs::read_to_string("res/opcodes-base.txt").unwrap();
+    let lines = contents.split("\n");
+    for line in lines {
+        let mut parts = line.split("\t");
+        let code =
+            usize::from_str_radix(parts.next().unwrap().strip_prefix("0x").unwrap(), 16).unwrap();
+        let mnem = parts.next().unwrap();
+        codes_from_file[code] = mnem.trim();
+    }
+
+    for i in 0..=0xFF {
+        println!(
+            "{i:#X?}:\n\tMine:    {:X?}\n\tCorrect: {}",
+            OP_TABLE[i], codes_from_file[i]
+        );
+    }
 }
