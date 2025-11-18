af0ea48469e081983bc3a8c57ea3583f8e4fefc3
Anthony Libardi <libardi42@gmail.com>
2025-11-17 23:24:10 -0500

    Oh, the wave ram range was wrong from v1.

--- src/gb/mmu/regions.rs
+++ src/gb/mmu/regions.rs
@@ -31,6 +31,28 @@ macro_rules! const_regions {
     };
 }
 
+const_regions!(
+    ALL_RAM:       0x0000, 0xFFFF
+    BOOT_ROM_BANK: 0x0000, 0x00FF
+    CART_HEADER:   0x0100, 0x014F
+    ROM_SPACE:     0x0000, 0x7FFF
+    ROM_BANK_0:    0x0000, 0x3FFF
+    ROM_BANK_N:    0x4000, 0x7FFF
+    VRAM:          0x8000, 0x9FFF
+    TILE_DATA:     0x8000, 0x97FF
+    TILE_MAPS:     0x9800, 0x9FFF
+    TILE_MAP_0:    0x9800, 0x9BFF
+    TILE_MAP_1:    0x9C00, 0x9FFF
+    EXTERNAL_RAM:  0xA000, 0xBFFF
+    WORK_RAM:      0xC000, 0xDFFF
+    ECHO_RAM:      0xE000, 0xFDFF
+    OAM:           0xFE00, 0xFE9F
+    UNUSABLE_MEM:  0xFEA0, 0xFEFF
+    HIGH_RAM:      0xFF00, 0xFFFF
+    IO_REGS:       0xFF00, 0xFF7F
+    IO_WAVE_RAM:   0xFF30, 0xFF3F
+);
+
 macro_rules! io_regs {
     ($($name:ident: $address:expr)*) => {
         $(
@@ -38,36 +60,6 @@ macro_rules! io_regs {
         )*
     };
 }
-
-const_regions!(
-    ALL_RAM:       0x0000, 0xFFFF
-
-    BOOT_ROM_BANK: 0x0000, 0x00FF
-    CART_HEADER:   0x0100, 0x014F
-
-    ROM_SPACE:     0x0000, 0x7FFF
-    ROM_BANK_0:    0x0000, 0x3FFF
-    ROM_BANK_N:    0x4000, 0x7FFF
-
-    VRAM:          0x8000, 0x9FFF
-    TILE_DATA:     0x8000, 0x97FF
-    TILE_MAPS:     0x9800, 0x9FFF
-    TILE_MAP_0:    0x9800, 0x9BFF
-    TILE_MAP_1:    0x9C00, 0x9FFF
-
-    EXTERNAL_RAM:  0xA000, 0xBFFF
-
-    WORK_RAM:      0xC000, 0xDFFF
-    ECHO_RAM:      0xE000, 0xFDFF
-
-    OAM:           0xFE00, 0xFE9F
-
-    UNUSABLE_MEM:  0xFEA0, 0xFEFF
-
-    HIGH_RAM:      0xFF00, 0xFFFF
-    IO_REGS:       0xFF00, 0xFF7F
-    IO_WAVE_RAM:   0xFF30, 0xFF40
-);
 
 io_regs!(
     IO_JOYP: 0xFF00
