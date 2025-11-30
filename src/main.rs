820e8492ff3c8cf2dea162d70013645c4b2bba59
Anthony Libardi <libardi42@gmail.com>
2025-11-30 15:21:23 -0500

    logger

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,18 @@
-fn main() {}
+use log::LevelFilter;
+use simple_logger::SimpleLogger;
+use std::env;
+
+fn main() {
+    // Set up logger
+    let args = env::args().collect::<Vec<_>>();
+    if args.len() > 1 {
+        env::set_var("RUST_LOG", &args[1]);
+    }
+    env::set_var("RUST_BACKTRACE", "1");
+
+    SimpleLogger::new()
+        .with_level(LevelFilter::Debug)
+        .env()
+        .init()
+        .unwrap();
+}
