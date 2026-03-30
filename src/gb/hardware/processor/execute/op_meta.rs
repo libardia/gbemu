5cb8b2051748338eb9c6e582d02773efde588b31
Anthony Libardi <libardi42@gmail.com>
2026-03-30 18:18:31 -0400

    reset :)

--- src/gb/hardware/processor/execute/op_meta.rs
+++
@@ -1,9 +0,0 @@
-use crate::{cpu_log, gb::GameBoy};
-
-pub fn terminate(ctx: &mut GameBoy) -> u16 {
-    cpu_log!(info, ctx, "TERMINATE instruction reached.");
-    ctx.exit = true;
-
-    // Instant (nothing else updates)
-    0
-}
