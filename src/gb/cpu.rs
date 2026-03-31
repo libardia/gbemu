6a4bcfe1ba3016935a0ae0cb8a74fecfdb551862
Anthony Libardi <libardi42@gmail.com>
2026-03-30 23:58:27 -0400

    once again working on cpu

---
+++ src/gb/cpu.rs
@@ -0,0 +1,102 @@
+#[derive(Default)]
+pub struct CPU {
+    // Registers
+    pub a: u8,
+    pub f: u8,
+
+    pub b: u8,
+    pub c: u8,
+
+    pub d: u8,
+    pub e: u8,
+
+    pub h: u8,
+    pub l: u8,
+}
+
+macro_rules! r16 {
+    ($r1:ident, $r2:ident) => {
+        paste::paste! {
+            pub fn [<get_ $r1 $r2>](&self) -> u16 {
+                (self.$r1 as u16) << 8 | self.$r2 as u16
+            }
+
+            pub fn [<set_ $r1 $r2>](&mut self, value: u16) {
+                self.$r1 = (value >> 8) as u8;
+                self.$r2 = (value & 0xFF) as u8
+            }
+        }
+    };
+}
+
+macro_rules! flag {
+    ($f:ident, $bit:literal) => {
+        paste::paste! {
+            const [<FLAG_ $f:upper _MASK>]: u8 = 1 << $bit;
+
+            pub fn [<get_flag_ $f>](&self) -> bool {
+                self.f & Self::[<FLAG_ $f:upper _MASK>] != 0
+            }
+
+            pub fn [<set_flag_ $f>](&mut self, value: bool) {
+                if value {
+                    self.f |= Self::[<FLAG_ $f:upper _MASK>];
+                } else {
+                    self.f &= !Self::[<FLAG_ $f:upper _MASK>];
+                }
+            }
+        }
+    };
+}
+
+impl CPU {
+    pub fn new() -> Self {
+        let cpu = Default::default();
+        // TODO: initializtion
+        cpu
+    }
+
+    r16!(a, f);
+    r16!(b, c);
+    r16!(d, e);
+    r16!(h, l);
+
+    flag!(z, 7);
+    flag!(n, 6);
+    flag!(h, 5);
+    flag!(c, 4);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::gb::cpu::CPU;
+
+    macro_rules! r16_test {
+        ($r1:ident, $r2:ident) => {
+            paste::paste! {
+                #[test]
+                fn [<r16_ $r1 $r2 _test>]() {
+                    let mut cpu = CPU::default();
+
+                    assert_eq!(cpu.$r1, 0);
+                    assert_eq!(cpu.$r2, 0);
+                    assert_eq!(cpu.[<get_ $r1 $r2>](), 0);
+
+                    cpu.[<set_ $r1 $r2>](0xDEAD);
+                    assert_eq!(cpu.$r1, 0xDE);
+                    assert_eq!(cpu.$r2, 0xAD);
+                    assert_eq!(cpu.[<get_ $r1 $r2>](), 0xDEAD);
+
+                    cpu.$r1 = 0xBE;
+                    cpu.$r2 = 0xEF;
+                    assert_eq!(cpu.[<get_ $r1 $r2>](), 0xBEEF);
+                }
+            }
+        };
+    }
+
+    r16_test!(a, f);
+    r16_test!(b, c);
+    r16_test!(d, e);
+    r16_test!(h, l);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,5 @@
+#![allow(dead_code)]
+
+mod gb;
+
 fn main() {}
